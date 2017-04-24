b3b4f281af3622d9bc1bdbc20e30619b4f789f41
Fiorellacr24 <fiorella.0624@gmail.com>
2017-04-23 23:51:25 -0500

    nav-contenido

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,24 +11,43 @@ var nav, div, logo, imglab,twitter, fb, edit, search;
 	imglab = document.createElement("img");
 	twitter = document.createElement("i");
 	fb = document.createElement("i");
-	edit = document.createElement("edit");
-	search = document.createElement("img");
-	s = document.createElement("img");
+	edit = document.createElement("span");
+	search = document.createElement("i");
+	s = document.createElement("span");
 	user = document.createElement("img");
 	ico = document.createElement("img");
 
 	logo.setAttribute("src", "assets/img/Medium-48.png");
 	imglab.setAttribute("src", "assets/img/laboratoria.png");
-	
+	twitter.setAttribute("class", "icon-twitter");
+	fb.setAttribute("class", "icon-facebook");
+	edit.innerText = "Edit";
+	search.setAttribute("class", "icon-search");
+	s.innerText = "S";
+	user.setAttribute("src", "assets/img/user.png");
+	ico.setAttribute("src", "assets/img/logo.png")
+
 	contenedor.prepend(nav);
 	nav.appendChild(div);
 	div.appendChild(logo);
 	div.appendChild(imglab);
+	div.appendChild(twitter);
+	div.appendChild(fb);
+	div.appendChild(edit);
+	div.appendChild(search);
+	div.appendChild(s);
+	div.appendChild(user);
+	div.appendChild(ico);
 
 	logo.style.width = "50px";
 
 	imglab.style.width = "90px";
 	imglab.style.margin = "10px 30px 5px";
+
+	user.style.width = "50px"; 
+
+	ico.style.width = "50px";
+
 		/*document.getElementById("contenido").innerHTML =
 		"<h1><b> Hazlo con estilo : Tipografías, iconos y sprite" + "</b> </h1>" +
 		"<p>Te imaginas el mundo sin tipos de letra, tamaños, colores, o sin iconos , nada, nada sería iguaaaaaal!!!! Pero gracias a muchos desarrolladores y diseñadores, hoy contamos con una inimaginable gama de fuentes para nuestro desarrollos web, pero la pregunta que nos hacemos es, ¿existen reglas, hay una mejor forma de hacerlo, no sé que tipografía usar? Tengo muchas preguntas y pocas respuestas!!" + "</p>";
